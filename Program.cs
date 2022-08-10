984523005c1774907e0ae13066c2cbeebc4bf64c
AlsuVel <alsu-hisamieva@mail.ru>
2022-08-10 03:09:35 +0300

    solution

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string[] inputArray = {"hello", "2", "world", ":-)"};
+
+for (int i =0; i< inputArray.Length; i++)
+{
+   var temp = inputArray[i];
+   if (temp.Length<=3)
+   {
+    Console.Write($"[{String.Join(";", temp)}]");
+   }
+}
+
